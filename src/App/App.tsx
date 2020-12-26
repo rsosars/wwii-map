7de998f1e4351c645f1b04b4f0a353b830cb683a
Kevin Le Goff <kev.legoff@gmail.com>
2020-12-26 19:30:48 +0200

    Add a simple map compnent

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Panel } from "./Components/Panel";
-import Map from "./Components/Map";
+import { Map } from "./Components/Map";
 import EventDetails from "./Components/EventDetails";
 export interface IAppProps extends React.ClassAttributes<IAppProps> {
 
@@ -31,7 +31,7 @@ export class App extends React.Component<IAppProps, IAppState> {
 		return(
 			<>
 				<div className="firstColumn">
-					<Map/>
+					<Map center={ {lat:52.5200, lng:13.4050}}/>
 					<EventDetails title="test" expanded={this.state.expanded} onClick={this.togglePanel}/>
 				</div>
 				<Panel></Panel>
